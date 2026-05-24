678b15f2872c7efefacaa2d8a65b21f46b60b844
MAK <mohd.ahmedkhan190@gmail.com>
2026-05-24 20:29:04 +0530

    getting started

---
+++ src/main/java/com/makafee/buybookapi/BuybookapiApplication.java
@@ -0,0 +1,13 @@
+package com.makafee.buybookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuybookapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuybookapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makafee/buybookapi/BuybookapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makafee.buybookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuybookapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
